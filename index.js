33d088de31b3362c7e5a666768eb4bfea5983b81
Ibra <ibrahimshoaib838@gmail.com>
2021-02-17 21:58:29 +0100

    CRUD - DELETE

--- index.js
+++ index.js
@@ -63,4 +63,11 @@ if (err) return res.send(500, err);
 res.redirect("/");
 });
 });
-    
+    //DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
